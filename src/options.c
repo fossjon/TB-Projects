d864d9af6eb81d6c8856f7e1265fc0097410ba59
Luis Rodrigo Gallardo Cruz <rodrigo@nul-unu.com>
2008-04-01 10:24:55 -0600

    Upstream 4.22
    sha1sum for upstream tarball: 452d0068bcae39afe7401216153986f3af810e37

--- src/options.c
+++ src/options.c
@@ -1,31 +1,38 @@
 /*
  *   stunnel       Universal SSL tunnel
- *   Copyright (c) 1998-2007 Michal Trojnara <Michal.Trojnara@mirt.net>
- *                 All Rights Reserved
+ *   Copyright (C) 1998-2008 Michal Trojnara <Michal.Trojnara@mirt.net>
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the
+ *   Free Software Foundation; either version 2 of the License, or (at your
+ *   option) any later version.
+ * 
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- *   In addition, as a special exception, Michal Trojnara gives
- *   permission to link the code of this program with the OpenSSL
- *   library (or with modified versions of OpenSSL that use the same
- *   license as OpenSSL), and distribute linked combinations including
- *   the two.  You must obey the GNU General Public License in all
- *   respects for all of the code used other than OpenSSL.  If you modify
- *   this file, you may extend this exception to your version of the
- *   file, but you are not obligated to do so.  If you do not wish to
- *   do so, delete this exception statement from your version.
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *   See the GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, see <http://www.gnu.org/licenses>.
+ * 
+ *   Linking stunnel statically or dynamically with other modules is making
+ *   a combined work based on stunnel. Thus, the terms and conditions of
+ *   the GNU General Public License cover the whole combination.
+ * 
+ *   In addition, as a special exception, the copyright holder of stunnel
+ *   gives you permission to combine stunnel with free software programs or
+ *   libraries that are released under the GNU LGPL and with code included
+ *   in the standard release of OpenSSL under the OpenSSL License (or
+ *   modified versions of such code, with unchanged license). You may copy
+ *   and distribute such a system following the terms of the GNU GPL for
+ *   stunnel and the licenses of the other code concerned.
+ * 
+ *   Note that people who make modified versions of stunnel are not obligated
+ *   to grant this special exception for their modified versions; it is their
+ *   choice whether to do so. The GNU General Public License gives permission
+ *   to release a modified version without this exception; this exception
+ *   also makes it possible to release a modified version which carries
+ *   forward this exception.
  */
 
 #include "common.h"
@@ -1079,11 +1086,39 @@ static char *service_options(CMD cmd, LOCAL_OPTIONS *section,
         break;
     }
 
+#ifndef USE_FORK
+    /* stack */
+    switch(cmd) {
+    case CMD_INIT:
+        section->stack_size=DEFAULT_STACK_SIZE;
+        break;
+    case CMD_EXEC:
+        if(strcasecmp(opt, "stack"))
+            break;
+        if(atoi(arg)>0)
+            section->stack_size=atoi(arg);
+        else
+            return "Illegal thread stack size";
+        return NULL; /* OK */
+    case CMD_DEFAULT:
+        s_log(LOG_RAW, "%-15s = %d bytes", "stack", section->stack_size);
+        break;
+    case CMD_HELP:
+        s_log(LOG_RAW, "%-15s = thread stack size (in bytes)", "stack");
+        break;
+    }
+#endif
+
     /* sslVersion */
     switch(cmd) {
     case CMD_INIT:
+#ifdef USE_FIPS
+        section->client_method=TLSv1_client_method;
+        section->server_method=TLSv1_server_method;
+#else
         section->client_method=SSLv3_client_method;
         section->server_method=SSLv23_server_method;
+#endif
         break;
     case CMD_EXEC:
         if(strcasecmp(opt, "sslVersion"))
@@ -1104,10 +1139,14 @@ static char *service_options(CMD cmd, LOCAL_OPTIONS *section,
             return "Incorrect version of SSL protocol";
         return NULL; /* OK */
     case CMD_DEFAULT:
+#ifdef USE_FIPS
+        s_log(LOG_RAW, "%-15s = TLSv1", "sslVersion");
+#else
         s_log(LOG_RAW, "%-15s = SSLv3 for client, all for server", "sslVersion");
+#endif
         break;
     case CMD_HELP:
-        s_log(LOG_RAW, "%-15s = all|SSLv2|SSLv3|TLSv1 SSL version", "sslVersion");
+        s_log(LOG_RAW, "%-15s = all|SSLv2|SSLv3|TLSv1 SSL method", "sslVersion");
         break;
     }
 
